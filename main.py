6c74baf953e1a8dd39a5d859f97474765f48390e
Will Taylor <w1ll100@hotmail.co.uk>
2018-05-01 18:34:54 +0100

    Moved slider code to function, deleted unused code, added more doc

--- main.py
+++ main.py
@@ -6,26 +6,110 @@ import math
 import vtk
 from numpy import fromfile
 
+#Builds the sliders and buttons
+def build_UI(pipeline, inital_thresh, min_thresh, max_thresh, filename_list):
+	print("Building UI")
+	#Values define sizes of sliders
+	tube_width = 0.022
+	slider_width = 0.04
+	endcap_width = 0.06
+	endcap_height = 0.003
 
+	#Slider to control the threshold value used by the ContourFilter
+	slider_thresh = vtk.vtkSliderRepresentation2D()
+	slider_thresh.SetMinimumValue(min_thresh)
+	slider_thresh.SetMaximumValue(max_thresh)
+	slider_thresh.SetValue(inital_thresh)
+	slider_thresh.SetTitleText("Isosurface threshold")
 
+	slider_thresh.GetPoint1Coordinate().SetCoordinateSystemToNormalizedDisplay()
+	slider_thresh.GetPoint1Coordinate().SetValue(.1, .9)
+	slider_thresh.GetPoint2Coordinate().SetCoordinateSystemToNormalizedDisplay()
+	slider_thresh.GetPoint2Coordinate().SetValue(.45, .9)
 
-#	TO DO:
-#	-Colours?
+	slider_thresh.SetTubeWidth(tube_width)
+	slider_thresh.SetSliderWidth(slider_width)
+	slider_thresh.SetEndCapWidth(endcap_width)
+	slider_thresh.SetEndCapLength(endcap_height)
 
+	slider_thresh_widget = vtk.vtkSliderWidget()
+	slider_thresh_widget.SetInteractor(pipeline["interactor"])
+	slider_thresh_widget.SetRepresentation(slider_thresh)
+	slider_thresh_widget.SetAnimationModeToAnimate()
+	slider_thresh_widget.EnabledOn()
+	slider_thresh_widget.SetNumberOfAnimationSteps(10)
 
+	#EndInteracitonEvent means the change occurs when the slider is released - this is necessary on larger datasets
+	slider_thresh_widget.AddObserver(vtk.vtkCommand.EndInteractionEvent, SliderThreshCallback(pipeline["contour"]))
+	#------------------------------------------------------------------------------
 
+	#Slider to control the number of images which are used too build the model
+	#Defaults to all images, reducing this creates a slicing effect
+	slider_cutoff = vtk.vtkSliderRepresentation2D()
+	slider_cutoff.SetMinimumValue(1)
+	slider_cutoff.SetMaximumValue(len(filename_list))
+	slider_cutoff.SetValue(len(filename_list))
+	slider_cutoff.SetTitleText("Slice")
 
+	slider_cutoff.GetPoint1Coordinate().SetCoordinateSystemToNormalizedDisplay()
+	slider_cutoff.GetPoint1Coordinate().SetValue(.55, .9)
+	slider_cutoff.GetPoint2Coordinate().SetCoordinateSystemToNormalizedDisplay()
+	slider_cutoff.GetPoint2Coordinate().SetValue(.95, .9)
+
+	slider_cutoff.SetTubeWidth(tube_width)
+	slider_cutoff.SetSliderWidth(slider_width)
+	slider_cutoff.SetEndCapWidth(endcap_width)
+	slider_cutoff.SetEndCapLength(endcap_height)
+
+	slider_cutoff_widget = vtk.vtkSliderWidget()
+	slider_cutoff_widget.SetInteractor(pipeline["interactor"])
+	slider_cutoff_widget.SetRepresentation(slider_cutoff)
+	slider_cutoff_widget.SetAnimationModeToAnimate()
+	slider_cutoff_widget.EnabledOn()
+	slider_cutoff_widget.SetNumberOfAnimationSteps(10)
+
+	slider_cutoff_widget.AddObserver(vtk.vtkCommand.EndInteractionEvent, SliderCutoffCallback(filename_list, pipeline["source"]))
+
+	return slider_thresh, slider_thresh_widget, slider_cutoff, slider_cutoff_widget
+
+#Callbacks for sliders
+class SliderThreshCallback():
+	def __init__(self, surface_extractor):
+		self.surface_extractor = surface_extractor
+
+	def __call__(self, caller, event):
+		slider_widget = caller
+		new_thresh = slider_widget.GetRepresentation().GetValue()
+		self.surface_extractor.SetValue(0, new_thresh)
+
+class SliderCutoffCallback():
+	def __init__(self, filename_list, reader):
+		self.filename_list = filename_list
+		self.reader = reader
+
+	def __call__(self, caller, event):
+		slider_widget = caller
+		#Values must be integers
+		slice_val = math.floor(slider_widget.GetRepresentation().GetValue())
+		caller.GetRepresentation().SetValue(slice_val)
+
+		self.filename_array = vtk.vtkStringArray()
+		self.filename_array.Initialize()
+		for filename in self.filename_list[:slice_val]:
+			self.filename_array.InsertNextValue(filename)
+		self.reader.SetFileNames(self.filename_array)
+		#Must implicitly state read has been modified in order to reread the data (with new set of images)
+		self.reader.Modified()
+
+#Determine value for isosurface automatically
+#Good values (found manually) for 3 test datasets are included
 def find_thresholds(filenames):
-	#Good values for test datasets:
-	# CThead 710 (head)/1150 (skull)
-	# MRBrain 1300
-	# bunny 1500
 	print("Determining contour threshold")
 
 	#Use good values for the 3 test datasets which were found manually
 	#Otherwise return 10% of the maximum voxel value, as recommended
 	max_val = 0
-	min_val = 70000
+	min_val = 65536
 	for filename in filenames:
 		#We create an array of unsigned short ints from the binary files
 		val_array = fromfile(filename, dtype = ">u2")
@@ -84,22 +168,11 @@ def main(image_width, image_height, filename_list):
 	pre_smoother = vtk.vtkImageGaussianSmooth()
 	pre_smoother.SetInputConnection(reader.GetOutputPort())
 
-	#Things we've tried, mention in report thne delete THIS OK ASDFASDFASDFASDFASDFASDFKAEDROGIMRO read me
-	#smoother.SetStandardDeviations(1.5, 1.5, 1.5)
-	#smoother.SetRadiusFactors(1.0, 1.0, 1.0)
-	# print(smoother.GetStandardDeviations())
-	# print(smoother.GetRadiusFactors())
-	#smoother = vtk.vtkImageMedian3D()
-	#smoother.SetInputConnection(reader.GetOutputPort())
-
 	#Create isosurface from voxel grid
-	#surface_extractor = vtk.vtkMarchingCubes()
-	surface_extractor = vtk.vtkContourFilter()
+	surface_extractor = vtk.vtkMarchingCubes()
+	#surface_extractor = vtk.vtkContourFilter()
 	surface_extractor.SetInputConnection(pre_smoother.GetOutputPort())
 	surface_extractor.SetValue(0, thresh)
-
-	#post_smoother = vtk.vtkSmoothPolyDataFilter()
-	#post_smoother.SetInputConnection(surface_extractor.GetOutputPort())
 
 	#Map polygon mesh to graphics primitives
 	surface_mapper = vtk.vtkPolyDataMapper()
@@ -110,15 +183,16 @@ def main(image_width, image_height, filename_list):
 	surface = vtk.vtkActor()
 	surface.SetMapper(surface_mapper)
 	surface.GetProperty().SetColor(0.7, 0.7, 0.7)
-	
 	surface.GetProperty().SetSpecular(0.25)
 	surface.GetProperty().SetSpecularPower(0.2)
-	#surface.GetProperty().Set
 
 	#Render and display actor(s)
 	renderer = vtk.vtkRenderer()
 	renderer.AddActor(surface)
 
+	#Set to True to produce 2 isosurface models.
+	#This code is specifically suited to the CThead dataset
+	#Could be extended to any number of surfaces (ContourFilter can easily handle multiple threshold values, for instance)
 	two_surface_model = False
 	if two_surface_model:
 		skull_extractor = vtk.vtkContourFilter()
@@ -131,8 +205,7 @@ def main(image_width, image_height, filename_list):
 
 		skull = vtk.vtkActor()
 		skull.SetMapper(skull_mapper)
-		skull.GetProperty().SetColor(0.6, 0.6, 0.6)
-		
+		skull.GetProperty().SetColor(0.6, 0.6, 0.6)		
 		skull.GetProperty().SetSpecular(0.2)
 		skull.GetProperty().SetSpecularPower(0.3)
 		skull.GetProperty().SetOpacity(1.0)
@@ -148,100 +221,35 @@ def main(image_width, image_height, filename_list):
 	interactor = vtk.vtkRenderWindowInteractor()
 	interactor.SetRenderWindow(window)
 
-	tube_width = 0.022
-	slider_width = 0.04
-	endcap_width = 0.06
-	endcap_height = 0.003
+	#Small data dict containing references to all the parts of the pipeline for convenience
+	pipeline = {"source"	:	reader,
+				"filter"	:	pre_smoother,
+				"contour"	:	surface_extractor,
+				"mapper"	:	surface_mapper,
+				"actor"		:	surface,
+				"renderer"	:	renderer,
+				"window"	:	window,
+				"interactor":	interactor}
 
-	slider_thresh = vtk.vtkSliderRepresentation2D()
-	slider_thresh.SetMinimumValue(min_image_val)
-	slider_thresh.SetMaximumValue(max_image_val)
-	slider_thresh.SetValue(thresh)
-	slider_thresh.SetTitleText("Isosurface threshold")
-
-	slider_thresh.GetPoint1Coordinate().SetCoordinateSystemToNormalizedDisplay()
-	slider_thresh.GetPoint1Coordinate().SetValue(.1, .9)
-	slider_thresh.GetPoint2Coordinate().SetCoordinateSystemToNormalizedDisplay()
-	slider_thresh.GetPoint2Coordinate().SetValue(.45, .9)
-
-	slider_thresh.SetTubeWidth(tube_width)
-	slider_thresh.SetSliderWidth(slider_width)
-	slider_thresh.SetEndCapWidth(endcap_width)
-	slider_thresh.SetEndCapLength(endcap_height)
-
-	slider_thresh_widget = vtk.vtkSliderWidget()
-	slider_thresh_widget.SetInteractor(interactor)
-	slider_thresh_widget.SetRepresentation(slider_thresh)
-	slider_thresh_widget.SetAnimationModeToAnimate()
-	slider_thresh_widget.EnabledOn()
-	slider_thresh_widget.SetNumberOfAnimationSteps(10)
-
-	slider_thresh_widget.AddObserver(vtk.vtkCommand.EndInteractionEvent, slider_thresh_callback(surface_extractor))
-
-	slider_cutoff = vtk.vtkSliderRepresentation2D()
-	slider_cutoff.SetMinimumValue(1)
-	slider_cutoff.SetMaximumValue(len(filename_list))
-	slider_cutoff.SetValue(len(filename_list))
-	slider_cutoff.SetTitleText("Slice")
-
-	slider_cutoff.GetPoint1Coordinate().SetCoordinateSystemToNormalizedDisplay()
-	slider_cutoff.GetPoint1Coordinate().SetValue(.55, .9)
-	slider_cutoff.GetPoint2Coordinate().SetCoordinateSystemToNormalizedDisplay()
-	slider_cutoff.GetPoint2Coordinate().SetValue(.95, .9)
-
-	slider_cutoff.SetTubeWidth(tube_width)
-	slider_cutoff.SetSliderWidth(slider_width)
-	slider_cutoff.SetEndCapWidth(endcap_width)
-	slider_cutoff.SetEndCapLength(endcap_height)
-
-	slider_cutoff_widget = vtk.vtkSliderWidget()
-	slider_cutoff_widget.SetInteractor(interactor)
-	slider_cutoff_widget.SetRepresentation(slider_cutoff)
-	slider_cutoff_widget.SetAnimationModeToAnimate()
-	slider_cutoff_widget.EnabledOn()
-	slider_cutoff_widget.SetNumberOfAnimationSteps(10)
-
-	slider_cutoff_widget.AddObserver(vtk.vtkCommand.EndInteractionEvent, slider_cutoff_callback(filename_list, filename_array, reader))
+	#Build sliders for UI. Lots of boilerplatey code, so were moved to separate function
+	slider_thresh, slider_thresh_widget, slider_cutoff, slider_cutoff_widget = build_UI(pipeline, thresh, min_image_val, max_image_val, filename_list)
 
 	interactor.Initialize()
 	interactor.Start()
 
-class slider_thresh_callback():
-	def __init__(self, surface_extractor):
-		self.surface_extractor = surface_extractor
-
-	def __call__(self, caller, event):
-		slider_widget = caller
-		new_thresh = slider_widget.GetRepresentation().GetValue()
-		self.surface_extractor.SetValue(0, new_thresh)
-
-class slider_cutoff_callback():
-	def __init__(self, filename_list, filename_array, reader):
-		self.filename_list = filename_list
-		self.filename_array = filename_array
-		self.reader = reader
-
-	def __call__(self, caller, event):
-		slider_widget = caller
-		slice_val = math.floor(slider_widget.GetRepresentation().GetValue())
-		caller.GetRepresentation().SetValue(slice_val)
-
-		self.filename_array.Initialize()
-		for filename in self.filename_list[:slice_val]:
-			self.filename_array.InsertNextValue(filename)
-
-		self.reader.SetFileNames(self.filename_array)
-		self.reader.Modified()
-
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description = 'Visualise images of 3D scans given as ')
+	parser = argparse.ArgumentParser(description = 'Visualise models of 3D scans from 2D images. Images must be rectangular binary images' + 
+													' (of the same size), with each voxel value specified by a big endian 2 byte value.\n\n' + 
+													'Image files can be given as a space separated list, or as a folder name which contains ONLY' + 
+													' the images. Images must be numbered from 1 to numberOfImages somewhere in the filename',
+									formatter_class = argparse.RawDescriptionHelpFormatter)
 	parser.add_argument('x', metavar = 'xPixelsPerImage', type = int, help = 'Width of images')
 	parser.add_argument('y', metavar = 'yPixelsPerImage', type = int, help = 'Height of images')
-	parser.add_argument('n', metavar = 'numberOfImages', type = int, help = 'Number of images / Depth of model')
-
+	parser.add_argument('n', metavar = 'numberOfImages', type = int, help = 'Number of images (Which defines the depth of the model)')
 	parser.add_argument('images', metavar = 'imageSource', nargs = "+", type = str, help = 'List of image names OR name of folder containing (only) images')
 	
 	args = parser.parse_args()
+	#Program designed to accept name of folder to iterate over, or the n individual images format which was specified
 	if len(args.images) == 1 or len(args.images) == args.n:
 		if len(args.images) == 1:
 			filename_list = [os.path.join(os.curdir, args.images[0], filename) for filename in os.listdir(args.images[0])]
